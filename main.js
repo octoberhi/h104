63351949037fc155bd98faaecd486c2ee1273a1a
octoberhi <89272971+octoberhi@users.noreply.github.com>
2022-01-21 16:48:44 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+Webcam.set({
+width:350,
+height:350,
+image_format:'png',
+png_quality:90
+});
+
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Q2bq13sQj/model.json',modelLoaded);
+
+
+function modelLoaded(){
+console.log("model is loaded");
+}
